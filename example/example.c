6bf842ffae4f64f9581c6f2a466a222cbb8b68f1
r0ck3r008 <naman.arora@tutanota.com>
2021-01-07 02:31:17 -0500

    Update example as per commit f55470e

--- example/example.c
+++ example/example.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 
-#include"objstore/ptree.h"
+#include "objstore/objstore.h"
 
 int main(int argc, char **argv)
 {
-    printf("Running!\n");
+    ObjStore *ost = objstore_init();
+    objstore_insert(ost, "naman", "arora");
+    objstore_insert(ost, "sakshu", "arora");
+    objstore_insert(ost, "nanan", "arora");
+    if (objstore_find(ost, "sakshu") != NULL)
+        printf("Found!\n");
+    objstore_deinit(ost);
 }
