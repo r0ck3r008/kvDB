7c532ebf7918eff408e86795b65578ce2c2ac089
r0ck3r008 <naman.arora@tutanota.com>
2021-01-07 18:09:22 -0500

    Example is to read dictionary and insert it

--- example/example.c
+++ example/example.c
@@ -1,14 +1,51 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<errno.h>
 
 #include "objstore/objstore.h"
 
-int main(int argc, char **argv)
+ObjStore *
+inputDictionary(FILE *f)
 {
     ObjStore *ost = objstore_init();
-    objstore_insert(ost, "naman", "arora");
-    objstore_insert(ost, "sakshu", "arora");
-    objstore_insert(ost, "nanan", "arora");
-    if (objstore_find(ost, "sakshu") != NULL)
-        printf("Found!\n");
+    char *line = NULL;
+    size_t n = 0;
+    char buf[512] = {0};
+    while(getline(&line, &n, f) != -1) {
+        snprintf(buf, n, "%s", line);
+        char *key = strtok(buf, ":");
+        char *value = strtok(NULL, ":");
+        objstore_insert(ost, key, value);
+
+        free(line);
+        line = NULL;
+        n = 0;
+        explicit_bzero(buf, sizeof(char) * 512);
+    }
+
+    return ost;
+}
+
+int
+main(int argc, char **argv)
+{
+    if(argc!=2) {
+        fprintf(stderr, "[-] Usage: ./example.out <path-to-dict-file>\n");
+        _exit(1);
+    }
+    FILE *f = NULL;
+    if((f=fopen(argv[1], "r")) == NULL) {
+        fprintf(stderr, "[-] Error in opening file %s: %s\n", argv[1], strerror(errno));
+        _exit(1);
+    }
+    ObjStore *ost = inputDictionary(f);
+    Pnode *ret = NULL;
+    if((ret=objstore_find(ost, "abate")) != NULL)
+        printf("Meaning: %s\n", ret->value);
+    else
+        printf("Not Found!\n");
     objstore_deinit(ost);
+    fclose(f);
 }
